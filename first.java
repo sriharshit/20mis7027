15602a4efde3afdc483a359c7f4c8d3e8cdaa972
harshith <harshith.20mis7027@vitap.ac.in>
2021-03-03 20:49:13 +0530

    made changes to first.java program

--- first.java
+++ first.java
@@ -1,1 +1,8 @@
-//This is my first file Hello.java 
+//This is my first file Hello.java
+import java.util.scanner;
+public class Main{
+	public static void main(String args[]){
+		system.out.println("hello world");
+	}
+}
+
